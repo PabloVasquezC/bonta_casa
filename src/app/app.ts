7f78e2813e5cd042632205f46a2af006205bb3e2
Pablo Vasquez <vascor.pablo@gmail.com>
2025-06-06 19:28:24 -0400

    footer added

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from "./global-components/nav-bar/nav-bar";
 import { HomePage } from "./home-page/home-page";
+import { Footer } from "./home-page/components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar],
+  imports: [RouterOutlet, NavBar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
   standalone: true,
