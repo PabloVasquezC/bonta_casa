5c279f5c840a2da12a49762f518bd85e64bcdb6e
Pablo Vasquez <vascor.pablo@gmail.com>
2025-06-26 16:50:04 -0400

    hero & description improving

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import 'zone.js';
+
 
 bootstrapApplication(App, {
   providers: [provideRouter(routes)],
